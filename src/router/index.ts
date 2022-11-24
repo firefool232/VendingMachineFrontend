2257ba5d4f1d5f358c4e85b65accff90fd56c2af
unknown <sergey.98@mail.ru>
2022-11-24 14:18:51 +0500

    Add guard for admin page

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter, { RouteConfig } from 'vue-router'
 import Vending from '../views/Vending.vue'
+import Admin from '../views/Admin.vue';
 
 Vue.use(VueRouter)
 
@@ -10,10 +11,26 @@ const routes: Array<RouteConfig> = [
     name: 'Vending',
     component: Vending
   },
+  {
+    path: '/admin',
+    name: 'Admin',
+    component: Admin
+  }
 ]
 
 const router = new VueRouter({
   routes
 })
+
+router.beforeEach((to, from, next) => {
+  if (to.name === 'Admin' && to.query.key != '256'){
+    next({
+      path: '/',
+      replace: true
+    })
+  } else {
+    next();
+  }
+});
 
 export default router
